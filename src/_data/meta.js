1dad1a6ed65309c4694af1154361e2296f55efce
Ben Myers <ben.myers@okstate.edu>
2023-10-11 10:59:28 -0500

    Update meta global data

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,8 +2,8 @@ module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
   url: process.env.URL || "http://localhost:8080",
-  siteName: "Smol 11ty Starter",
+  siteName: "Eleventy Blogroll Demo",
   siteDescription:
-    "Extremely minimal Eleventy starter to kickstart a simple multi-page site / a nearly opinionless foundation to continue building on.",
-  authorName: "Stephanie Eckles", // optional
+    "Showcase your favorite blogs and their latest posts with this daily blogroll, demoed at the Eleventy Meetup",
+  authorName: "Ben Myers", // optional
 };
